055c3a72c858e22cd15962ba5e379840cb66dc98
Anzle Chavez <anzlemchavez@gmail.com>
2024-04-07 13:20:00 -0400

    Calculation for gravitational constant

--- src/force/gravity.rs
+++ src/force/gravity.rs
@@ -1,13 +1,15 @@
-use super::{k_mE, k_rE, Force};
+use super::{k_G, k_mE, k_rE, Force};
 pub struct Gravity {
-    m: f32,
-    d: f32,
+    // Gravitational constant without the objects mass
+    k: f32
 }
 
 impl Gravity {
     /// Create a gravity force with the mass of the surface and the distance from the center.
     pub fn new(m: f32, d: f32) -> Self {
-        Self { m, d }
+        Self {
+            k: calculate_grav(m, d)
+        }
     }
 }
 
@@ -23,3 +25,6 @@ impl Force for Gravity {
     }
 }
 
+pub fn calculate_grav(m: f32, d: f32) -> f32 {
+    (k_G * m) / f32::powi(d, 2)
+}
