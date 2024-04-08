b49f62e597bc8e6d168c56057cb052bc5fea8f89
Anzle Chavez <anzlemchavez@gmail.com>
2024-04-08 13:41:30 -0400

    Removed unnecessary import in app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,3 @@
-use std::{ops::Add, vec};
-
 use nannou::{draw::mesh::vertex::Color, prelude::*};
 use nannou_egui::{
     self,
@@ -13,6 +11,7 @@ use neophys::{
     },
     engine::{self, Engine},
 };
+use std::{borrow::Borrow, cell::RefCell, ops::Add, vec};
 
 static TITLE: &str = "Root Viewport";
 static V1_TITLE: &str = "Viewport 1";
@@ -52,14 +51,14 @@ fn model(app: &App) -> Model {
         egui,
         settings: Settings {
             resolution: 10,
-            scale: 50.0,
+            scale: 25.0,
             color: WHITE,
             time: 100.0,
         },
         x: 0.0,
         t: 0.0,
         m: 10.0,
-        engine: Engine::new(vec![Box::new(Body::new(BodyType::GPoint, 10.0))]),
+        engine: Engine::new(vec![RefCell::new(Body::new(BodyType::GPoint, 10.0))]),
     }
 }
 
@@ -73,13 +72,23 @@ fn update(_app: &App, model: &mut Model, update: Update) {
     egui::Window::new("Settings").show(frame, |ui| {
         ui.label("Mass of body");
         ui.add(egui::Slider::new(&mut model.m, 0.1..=100.0).text("Mass"));
+        ui.label(format!(
+            "Mass (kg): {}",
+            model.engine.bodies().get(0).unwrap().borrow().mass()
+        ));
+        ui.label(format!(
+            "Gravity force on the object: {}",
+            model.engine.body(0).unwrap().borrow_mut().sum_forces()
+        ))
     });
 
-    if model.engine.peek_bodies().first().unwrap().mass() != model.m {
-        model.engine.update_mass(model.m, 0);
+    if model.engine.peek_bodies().first().unwrap().borrow_mut().m != model.m {
+        model.engine.update_mass(model.m, 0).ok();
     };
 
     model.t = _app.elapsed_frames() as f32 / model.settings.time;
+
+    model.engine.calc(model.t)
 }
 
 fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
@@ -89,11 +98,9 @@ fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event:
 fn view(app: &App, model: &Model, frame: Frame) {
     let settings = &model.settings;
     let mut engine = &model.engine;
-
+    let win = app.window_rect();
     let draw = app.draw();
     draw.background().color(srgba(0.0, 0.0, 0.0, 1.0));
-
-    engine.calc(model.t);
     println!("{:?}", engine);
 
     draw.ellipse()
@@ -101,6 +108,11 @@ fn view(app: &App, model: &Model, frame: Frame) {
         .xy(vec2(model.x, 0.0))
         .color(settings.color)
         .radius(settings.scale);
+
+    draw.line()
+        .weight(1.0)
+        .color(WHITE)
+        .points(win.mid_left(), win.mid_right());
 
     draw.to_frame(app, &frame).unwrap();
     model.egui.draw_to_frame(&frame).unwrap();
